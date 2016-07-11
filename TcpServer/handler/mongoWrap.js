363c6edbef0893070905181822a3cab8a642409b
txx3258 <soft_tangxiaoxian@163.com>
2016-07-11 19:49:43 +0800

    优化

--- TcpServer/handler/mongoWrap.js
+++ TcpServer/handler/mongoWrap.js
@@ -11,7 +11,7 @@ function mongoWrap(data) {
         let preData = tmpData[key];
         let isDirty = false;
         if (preData) {
-            if (preData.length > 10000) {
+            if (preData.length > 2000) {
                 console.log('miss data:' + preData);
                 tmpData[key]=null;
                 delete tmpData[key];
