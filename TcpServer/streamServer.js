7921a4b3a3d90c907e4bbc1c8cbcfdbd5d72d8df
txx3258 <soft_tangxiaoxian@163.com>
2016-07-13 09:35:55 +0800

    sever超时

--- TcpServer/streamServer.js
+++ TcpServer/streamServer.js
@@ -12,8 +12,11 @@ function createStreamServer() {
         //暂存Buffer
         let buf = {};
         //接受数据
+        let identifyMap = {};
         socket.on('data', function (buffer) {
             let identify = socket.remoteAddress + '_' + socket.remotePort;
+            identifyMap[identify]=new Date().getTime();
+
             let id_buf = buf[identify];
             if (!id_buf) {
                 id_buf = [];
@@ -26,10 +29,18 @@ function createStreamServer() {
 
         //处理数据
         socket.on('done', function () {
-            console.log(JSON.stringify(buf));
+            //console.log(JSON.stringify(buf));
+            let now = new Date().getTime();
+            Object.keys(identifyMap).forEach((key)=>{
+                if (now-identifyMap[key]>2000){
+                    console.log(buf[key].join('').toString('utf8'));
+                    buf[key]=[];
+                }
+            });
+            console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
 
             // 单线程能确保安全性
-            buf = {};
+            //buf = {};
         });
 
         //结束
