8584bc780e9a76a7ff60feddba88efb06ddf2d8f
txx3258 <soft_tangxiaoxian@163.com>
2016-07-26 11:32:17 +0800

    [[ 出错shell

--- TcpServer/stop.sh
+++ TcpServer/stop.sh
@@ -9,7 +9,7 @@ PID_JSON `ps -ef|grep node|grep jsonServer.js|awk '{print $2}'`
 PID_STREAM `ps -ef|grep node|grep streamServer.js|awk '{print $2}'`
 
 echo -e "Kill PID_JSON=$PID_JSON,PID_STREAM=$PID_STREAM"
-if [ "$PID_JSON" !=  ""]||["$PID_STREAM" !=  "" ] ;then
+if [[ "$PID_JSON" !=  ""]]||[["$PID_STREAM" !=  "" ]] ;then
   kill ${PID_JSON}
   kill ${PID_STREAM}
 
@@ -18,7 +18,7 @@ if [ "$PID_JSON" !=  ""]||["$PID_STREAM" !=  "" ] ;then
 	  sleep 1;
 	  echo -e "Wait for Exit (${i}s)."
 	
-	  if [ $(ps -fp ${PID_JSON} 2>/dev/null | grep -c 'jsonServer') -eq 0 ]&&[ $(ps -fp ${PID_STREAM} 2>/dev/null | grep -c 'streamServer') -eq 0 ];then
+	  if [[ $(ps -fp ${PID_JSON} 2>/dev/null | grep -c 'jsonServer') -eq 0 ]]&&[[ $(ps -fp ${PID_STREAM} 2>/dev/null | grep -c 'streamServer') -eq 0 ]];then
 		echo -e "Success Kill PID_JSON=$PID_JSON,PID_STREAM=$PID_STREAM Done."
 		break;
 	  fi	
