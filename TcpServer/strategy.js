422c1556b8c34ed661185a9e59474edc8888d18c
txx3258 <tangxx3@LENOVO.COM>
2016-08-17 11:27:12 +0800

    node更改为6

--- TcpServer/strategy.js
+++ TcpServer/strategy.js
@@ -1,9 +1,9 @@
 'use strict';
 
-let config = require('../config');
+let conf = require('./config');
 let child_process = require('child_process');
-let STREAM_CHILED_NUM = 1;
-let JSON_CHILED_NUM = 1;
+let STREAM_CHILED_NUM = conf.STREAM_CHILED_NUM;
+let JSON_CHILED_NUM = conf.JSON_CHILED_NUM;
 
 /**
  * 选择配置文件中IP与机器IP一致
