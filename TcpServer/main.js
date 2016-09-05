4b2bbe1e748ca669300985fd7a4da3da0fb94e7f
txx3258 <tangxx3@LENOVO.COM>
2016-09-05 10:43:45 +0800

    修改配置

--- TcpServer/main.js
+++ TcpServer/main.js
@@ -4,7 +4,7 @@ let child_process = require('child_process');
 
 let child = require('child_process');
 let jsonServer = child_process.fork('./TcpServer/server.js', ['json'], {encoding:'utf8' });
-let streamServer = child_process.fork('./TcpServer/server.js', ['stream'], {encoding:'utf8'});
+//let streamServer = child_process.fork('./TcpServer/server.js', ['stream'], {encoding:'utf8'});
 
 let jsonCount ={time:new Date().getTime()};
 let streamCount ={time:new Date().getTime()};
@@ -18,13 +18,13 @@ jsonServer.on('message', function(m) {
   }
 });
 
-streamServer.on('message', function(m) {
-  if (m.action=='heart'){
-    streamCount = m;
-  }else{
-    console.log(m);
-  }
-});
+// streamServer.on('message', function(m) {
+//   if (m.action=='heart'){
+//     streamCount = m;
+//   }else{
+//     console.log(m);
+//   }
+// });
 
 //****************监控****************************/
 setInterval(()=>{
@@ -34,11 +34,11 @@ setInterval(()=>{
     console.log('restart json server');
   }
 
-  if (time - jsonCount.time>10000){
-     streamServer = child_process.fork('./TcpServer/server.js', ['stream'], {encoding:'utf8'});
-     console.log('restart stream server');
-  } 
+  // if (time - jsonCount.time>10000){
+  //   // streamServer = child_process.fork('./TcpServer/server.js', ['stream'], {encoding:'utf8'});
+  //    console.log('restart stream server');
+  // } 
 
   jsonServer.send({time:time,action:'heart'});
-  streamServer.send({time:time,action:'heart'});
+//  streamServer.send({time:time,action:'heart'});
 },2000)
